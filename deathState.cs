abc21594c2631a5df986a08d9869d5428df6cfe2
RossTheDev <31748549+RossTheDev@users.noreply.github.com>
2018-10-08 07:55:17 -0500

    Add files via upload

---
+++ deathState.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class deathState : MonoBehaviour
+{
+    [SerializeField] private string newLevel;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(newLevel);
+            ScoreScript.scoreValue = 0;
+
+        }
+    }
+}
+
+//switches scenes and resets score (mostly for fall deaths, sep script already made for spikes)
