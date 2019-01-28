37efefaec9474d637bf33bef264d6a3bf286fed8
RossTheDev <31748549+RossTheDev@users.noreply.github.com>
2019-01-27 19:30:39 -0600

    Add files via upload

---
+++ ExitScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ExitScript : MonoBehaviour
+{
+    [SerializeField] private string newLevel;
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(newLevel);
+
+        }
+    }
+}
